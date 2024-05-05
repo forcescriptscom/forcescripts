785b21ec1422121524b863d41db9aec6393ceb71
Gaffar Korkmaz <134064540+forcescriptscom@users.noreply.github.com>
2024-05-05 17:30:06 +0300

    Update veritabani.sql

--- update/epinsatis/veritabani.sql
+++ update/epinsatis/veritabani.sql
@@ -1,1 +1,23 @@
-INSERT INTO `config` (`name`, `value`) VALUES ('dc_client_id', ''), ('dc_client_secret', ''), ('h_site_key', ''), ('h_secret_key', '');
+INSERT INTO `config` (`name`, `value`)
+SELECT * FROM (SELECT 'dc_client_id', '') AS tmp
+WHERE NOT EXISTS (
+    SELECT `name` FROM `config` WHERE `name` = 'dc_client_id'
+) LIMIT 1;
+
+INSERT INTO `config` (`name`, `value`)
+SELECT * FROM (SELECT 'dc_client_secret', '') AS tmp
+WHERE NOT EXISTS (
+    SELECT `name` FROM `config` WHERE `name` = 'dc_client_secret'
+) LIMIT 1;
+
+INSERT INTO `config` (`name`, `value`)
+SELECT * FROM (SELECT 'h_site_key', '') AS tmp
+WHERE NOT EXISTS (
+    SELECT `name` FROM `config` WHERE `name` = 'h_site_key'
+) LIMIT 1;
+
+INSERT INTO `config` (`name`, `value`)
+SELECT * FROM (SELECT 'h_secret_key', '') AS tmp
+WHERE NOT EXISTS (
+    SELECT `name` FROM `config` WHERE `name` = 'h_secret_key'
+) LIMIT 1;
